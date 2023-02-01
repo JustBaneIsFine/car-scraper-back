9d5143be3cd7206a324e153b77ff26eea4517c1b
Bane <bane.baki.1997@gmail.com>
2023-02-01 16:09:33 +0100

    Testing vercel function timeout

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.json({ name: 'hellooo222o' });
+  let x = 'It works after 5 seconds';
+  setTimeout(() => {
+    res.send(x);
+  }, 6000);
 });
 
 export default router;
--- api/users.ts
+++ api/users.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resourceeeeee');
+  let x = 'It works after 10 seconds';
+  setTimeout(() => {
+    res.send('');
+  }, 11000);
 });
 
 export default router;
