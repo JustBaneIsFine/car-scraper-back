686034cebed3bba2ff8f6d5de3d506c7b19b9152
Bane <bane.baki.1997@gmail.com>
2023-02-01 16:17:34 +0100

    Test finished, 10 second limit

--- api/index.ts
+++ api/index.ts
@@ -4,10 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  let x = 'It works after 5 seconds';
-  setTimeout(() => {
-    res.send(x);
-  }, 6000);
+ res.send('Index page')
 });
 
 export default router;
--- api/users.ts
+++ api/users.ts
@@ -3,10 +3,7 @@ import express from 'express';
 const router = express.Router();
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  let x = 'It works after 10 seconds';
-  setTimeout(() => {
-    res.send('');
-  }, 11000);
+  res.send('Users page');
 });
 
 export default router;
