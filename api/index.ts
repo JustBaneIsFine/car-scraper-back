1022cf1bdca39faaafe36ac0cb92407373896922
Bane <bane.baki.1997@gmail.com>
2023-02-02 17:16:55 +0100

    Testing puppeteer

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
- res.send('Index page')
+  res.send('Index page');
 });
 
 export default router;
--- api/ts/scrapeTest.ts
+++ api/ts/scrapeTest.ts
@@ -1,7 +1,41 @@
-import puppeteer from 'puppeteer';
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+
+const exePath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
+
+async function getBrowser() {
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+      }
+    : {
+        args: [],
+        executablePath: exePath,
+        headless: true,
+      };
+
+  const browser = await puppeteer.launch(options);
+  return browser;
+}
 
 export default async function scrapeTest() {
-  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch();
+  // const page = await browser.newPage();
+  // await page.goto('https://google.com', { waitUntil: 'load' });
+  // const result = await page.evaluate(() => {
+  //   const list = document.getElementsByTagName('a');
+  //   const array: string[] = [];
+
+  //   for (let i = 0; i < list.length; i += 1) {
+  //     array.push(list[i].innerText);
+  //   }
+  //   return array;
+  // });
+
+  // await browser.close();
+  const browser = await getBrowser();
   const page = await browser.newPage();
   await page.goto('https://google.com', { waitUntil: 'load' });
   const result = await page.evaluate(() => {
@@ -13,8 +47,5 @@ export default async function scrapeTest() {
     }
     return array;
   });
-
-  await browser.close();
-
   return result;
 }
