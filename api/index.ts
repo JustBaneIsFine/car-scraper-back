02cb86cea852ac48a1af376053fe2e350206c6bd
Bane <bane.baki.1997@gmail.com>
2023-02-02 18:09:00 +0100

    checking version on vercel

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('Index page');
+  res.send(`Index page and node version: ${process.version}`);
 });
 
 export default router;
