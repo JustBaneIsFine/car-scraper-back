7ba7f14e18ce3e31df95cb043becba57d08472fe
Bane <bane.baki.1997@gmail.com>
2023-02-14 13:32:34 +0100

    removing comments and adding new route

--- server.ts
+++ server.ts
@@ -4,26 +4,14 @@ import cors from 'cors';
 
 import indexRouter from './api/index';
 import usersRouter from './api/users';
-import testScraping from './api/testScraping';
+import scrapeRouter from './api/scrape';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-// let isDevMode = false;
-// if (process.env.USERNAME === 'Theseus') {
-//   isDevMode = true;
-// } else {
-//   isDevMode = false;
-// }
-// console.log(process.env.USERNAME);
-// // app.use(logger('dev'));
-// const devOrigin = 'http://localhost:5173';
-// const prodOrigin = 'https://car-scraper.netlify.app';
-
 app.use(
   cors({
     origin: 'https://car-scraper.netlify.app',
-    // origin: isDevMode ? devOrigin : prodOrigin,
   })
 );
 app.use(express.json());
@@ -48,7 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/test', testScraping);
+app.use('/scrape', scrapeRouter);
 app.use((req, res) => {
   res.send('error2');
 });
