ece0735693b1ee4d8dc8d7037e7504092f3180b2
Bane <bane.baki.1997@gmail.com>
2023-02-15 15:54:15 +0100

    testing polovni

--- api/ts/puppeteer.ts
+++ api/ts/puppeteer.ts
@@ -1,34 +1,33 @@
 import chrome from 'chrome-aws-lambda';
-import { addExtra } from 'puppeteer-extra';
+import puppeteer from 'puppeteer-core';
 
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.app';
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.csi';
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes';
-import 'puppeteer-extra-plugin-stealth/evasions/chrome.runtime';
-import 'puppeteer-extra-plugin-stealth/evasions/defaultArgs';
-import 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow';
-import 'puppeteer-extra-plugin-stealth/evasions/media.codecs';
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency';
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.languages';
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions';
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins';
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor';
-import 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver';
-import 'puppeteer-extra-plugin-stealth/evasions/sourceurl';
-import 'puppeteer-extra-plugin-stealth/evasions/user-agent-override';
-import 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor';
-import 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions';
-import 'puppeteer-extra-plugin-user-preferences';
-import 'puppeteer-extra-plugin-user-data-dir';
+// import { addExtra } from 'puppeteer-extra';
+// import 'puppeteer-extra-plugin-stealth/evasions/chrome.app';
+// import 'puppeteer-extra-plugin-stealth/evasions/chrome.csi';
+// import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes';
+// import 'puppeteer-extra-plugin-stealth/evasions/chrome.runtime';
+// import 'puppeteer-extra-plugin-stealth/evasions/defaultArgs';
+// import 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow';
+// import 'puppeteer-extra-plugin-stealth/evasions/media.codecs';
+// import 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency';
+// import 'puppeteer-extra-plugin-stealth/evasions/navigator.languages';
+// import 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions';
+// import 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins';
+// import 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor';
+// import 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver';
+// import 'puppeteer-extra-plugin-stealth/evasions/sourceurl';
+// import 'puppeteer-extra-plugin-stealth/evasions/user-agent-override';
+// import 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor';
+// import 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions';
+// import 'puppeteer-extra-plugin-user-preferences';
+// import 'puppeteer-extra-plugin-user-data-dir';
 
-import stealthPlugin from 'puppeteer-extra-plugin-stealth';
+// import stealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 export default async function getBrowser() {
-  const puppeteerExtra = addExtra(chrome.puppeteer);
-
-  const stealth = stealthPlugin();
-
-  puppeteerExtra.use(stealth);
+  // const puppeteerExtra = addExtra(chrome.puppeteer);
+  // const stealth = stealthPlugin();
+  // puppeteerExtra.use(stealth);
 
   const exePath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
 
@@ -45,6 +44,6 @@ export default async function getBrowser() {
         headless: true,
       };
 
-  const browser = await puppeteerExtra.launch(options);
+  const browser = await puppeteer.launch(options);
   return browser;
 }
