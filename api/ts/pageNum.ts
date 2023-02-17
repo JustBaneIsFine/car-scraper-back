7aa4f8692e91e7968cc48f812e160478226351af
Bane <bane.baki.1997@gmail.com>
2023-02-17 12:54:39 +0100

    testing

--- api/ts/pageNum.ts
+++ api/ts/pageNum.ts
@@ -66,27 +66,30 @@ async function polovniPageNum(url: string): Promise<number | false> {
   const browser = await getBrowser();
   const page = await browser.newPage();
   let num: number | false = 0;
-
+  console.log('waiting for response');
   await Promise.all([
     page.waitForResponse(async (response) => {
       if (
         response.url().includes('pretraga?') &&
         response.headers()['content-type'] === 'text/html; charset=UTF-8'
       ) {
+        console.log('found response');
         // load html with cheerio
         const dataHtml = await response.text();
-
+        console.log('loaded text');
         const $ = cheerio.load(dataHtml);
 
         const text = $('.reversre-search').text();
+        console.log('found text');
         if (text !== null && text.includes('Trenutno nema rezultata')) {
           num = false;
           return true;
         }
-
+        console.log('text is good');
         const smallText = $(
           'div.js-hide-on-filter:nth-child(3) > small:nth-child(1)'
         );
+        console.log('found smallText');
         if (smallText !== null && smallText !== undefined) {
           const numOfAds = $(smallText).text();
           const number: number = parseInt(
@@ -94,13 +97,17 @@ async function polovniPageNum(url: string): Promise<number | false> {
             10
           );
           num = Math.ceil(number / 25);
+          console.log('small text ');
           return true;
         }
+        console.log('small text not good');
         return true;
       }
       return false;
     }),
     await page.goto(url, { waitUntil: 'domcontentloaded' }),
   ]);
+  console.log('finished everything');
+
   return num;
 }
