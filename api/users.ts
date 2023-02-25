448887ba24d96560b8a5a1097baece7a503ac3ce
Bane <bane.baki.1997@gmail.com>
2023-02-25 09:06:23 +0100

    removing unnecessary files

--- api/users.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('Users page');
-});
-
-export default router;
