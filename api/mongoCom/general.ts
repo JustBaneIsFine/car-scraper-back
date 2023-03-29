259ea2332b28fa3d2fae757ac98a787dade0c38c
Bane <bane.baki.1997@gmail.com>
2023-03-29 19:45:27 +0200

    refactoring and testing completed

--- api/mongoCom/general.ts
+++ api/mongoCom/general.ts
@@ -94,6 +94,7 @@ export async function findUserByName(
     searchType: 'username',
     searchValue: username,
   })) as UserUnsafeFull | false;
+
   return result;
 }
 export async function findSessionByUsername(
@@ -125,28 +126,15 @@ export async function findCarPost(
 async function updateDocumentFunc(
   client: MongoClient,
   data: {
-    collection: 'Users' | 'UsersCars' | 'Sessions';
+    collection: 'Users' | 'UsersCars';
     searchType: 'username' | 'Id';
     searchValue: string;
     data: any;
-    searchType2?: string;
-    searchValue2?: string;
-    setType?: 'post' | 'favorite';
   }
 ) {
   const filter = { [data.searchType]: data.searchValue };
   const setData = { $set: data.data };
-
-  if (data.setType === 'post') {
-    setData.$set = { 'posts.$': data.data };
-  } else if (data.setType === 'favorite') {
-    setData.$set = { 'favorites.$': data.data };
-  }
-
-  if (data.searchType2 && data.searchValue2 && data.setType) {
-    filter[data.searchType2] = data.searchValue2;
-  }
-
+  console.log('filter and set data for single', filter, setData);
   const item = await client
     .db(database)
     .collection(data.collection)
@@ -155,13 +143,10 @@ async function updateDocumentFunc(
   return item;
 }
 export async function updateDocument(data: {
-  collection: 'Users' | 'UsersCars' | 'Sessions';
-  searchType: 'username' | 'Id' | 'posts.Id' | 'favorites.Id';
+  collection: 'Users' | 'UsersCars';
+  searchType: 'username' | 'Id';
   searchValue: string;
   data: any;
-  searchType2?: string;
-  searchValue2?: string;
-  setType?: 'post' | 'favorite';
 }) {
   const result = await mainCommFunc(updateDocumentFunc, data);
   return result;
@@ -170,27 +155,15 @@ export async function updateDocument(data: {
 async function updateDocumentsFunc(
   client: MongoClient,
   data: {
-    collection: 'Users' | 'UsersCars' | 'Sessions';
-    searchType: 'username' | 'Id';
+    collection: 'Users' | 'Sessions';
+    searchType: 'post' | 'favorite';
     searchValue: string;
     data: any;
-    searchType2?: string;
-    searchValue2?: string;
-    setType?: 'post' | 'favorite';
   }
 ) {
-  const filter = { [data.searchType]: data.searchValue };
-  const setData = { $set: data.data };
-
-  if (data.setType === 'post') {
-    setData.$set = { 'posts.$': data.data };
-  } else if (data.setType === 'favorite') {
-    setData.$set = { 'favorites.$': data.data };
-  }
-
-  if (data.searchType2 && data.searchValue2 && data.setType) {
-    filter[data.searchType2] = data.searchValue2;
-  }
+  const searchType = data.searchType === 'post' ? 'posts.Id' : 'favorites.Id';
+  const filter = { [searchType]: data.searchValue };
+  const setData = convertToMongoSetData(data.searchType, data.data);
 
   const item = await client
     .db(database)
@@ -200,13 +173,10 @@ async function updateDocumentsFunc(
   return item;
 }
 export async function updateDocuments(data: {
-  collection: 'Users' | 'UsersCars' | 'Sessions';
-  searchType: 'username' | 'Id' | 'posts.Id' | 'favorites.Id';
+  collection: 'Users' | 'Sessions';
+  searchType: 'post' | 'favorite';
   searchValue: string;
   data: any;
-  searchType2?: string;
-  searchValue2?: string;
-  setType?: 'post' | 'favorite';
 }) {
   const result = await mainCommFunc(updateDocumentsFunc, data);
   return result;
@@ -223,14 +193,12 @@ async function addObjectToArrayFunc(
     setType: 'post' | 'favorites';
   }
 ) {
-  // this function adds a whole object to an array
-  // it takes a CarObject and pushes it to the posts or favorites fields
   const filter = { [data.searchType]: data.searchValue };
   let setData;
   if (data.setType === 'post') {
-    setData = { $push: { 'posts.$': data.data } };
+    setData = { $push: { posts: data.data } };
   } else {
-    setData = { $push: { 'favorites.$': data.data } };
+    setData = { $push: { favorites: data.data } };
   }
 
   const item = await client
@@ -248,7 +216,7 @@ export async function addObjectToDocument(data: {
   data: any;
   searchType2?: string;
   searchValue2?: string;
-  setType?: 'post' | 'favorite';
+  setType: 'post' | 'favorite';
 }) {
   const result = await mainCommFunc(addObjectToArrayFunc, data);
   return result;
@@ -267,10 +235,10 @@ async function deleteDocumentFunc(
   }
 ) {
   const filter = { [data.searchType]: data.searchValue };
-
   if (data.searchType2 && data.searchValue2) {
     filter[data.searchType2] = data.searchValue2;
   }
+
   const item = await client
     .db(database)
     .collection(data.collection)
@@ -333,7 +301,10 @@ async function deleteFromArrayFunc(
   }
 ) {
   const deleteKey = data.deleteKey === 'post' ? 'posts.Id' : 'favorites.Id';
-  const deleteValue = data.deleteKey === 'post' ? 'posts.$' : 'favorites.$';
+  const deleteThis =
+    data.deleteKey === 'post'
+      ? { posts: { Id: data.deleteValue } }
+      : { favorites: { Id: data.deleteValue } };
 
   const item = await client
     .db(database)
@@ -343,10 +314,10 @@ async function deleteFromArrayFunc(
         [data.searchType]: data.searchValue,
         [deleteKey]: data.deleteValue,
       },
-      { $pull: deleteValue }
+      { $pull: deleteThis }
     );
 
-  if (!item.modifiedCount) return false;
+  if (!item.modifiedCount && item.matchedCount) return false;
   return true;
 }
 async function deleteFromArrayMultipleFunc(
@@ -360,20 +331,21 @@ async function deleteFromArrayMultipleFunc(
   }
 ) {
   const deleteKey = data.deleteKey === 'post' ? 'posts.Id' : 'favorites.Id';
-  const deleteValue = data.deleteKey === 'post' ? 'posts.$' : 'favorites.$';
-
+  const deleteThis =
+    data.deleteKey === 'post'
+      ? { posts: { Id: data.deleteValue } }
+      : { favorites: { Id: data.deleteValue } };
   const item = await client
     .db(database)
     .collection(data.collection)
     .updateOne(
       {
-        [data.searchType]: data.searchValue,
         [deleteKey]: data.deleteValue,
       },
-      { $pull: deleteValue }
+      { $pull: deleteThis }
     );
 
-  if (!item.modifiedCount) return false;
+  if (!item.modifiedCount && item.matchedCount) return false;
   return true;
 }
 export async function deleteFromArray(data: {
@@ -397,356 +369,6 @@ export async function deleteFromArrayMultiple(data: {
   const result = await mainCommFunc(deleteFromArrayMultipleFunc, data);
   return result;
 }
-//
-//
-//
-//
-//
-//
-//
-//
-// async function createUserComm(
-//   client: MongoClient,
-//   data: UserBasic
-// ): Promise<ObjectId | false> {
-//   const result = await client.db(database).collection(userColl).insertOne(data);
-//   if (!result) {
-//     return false;
-//   }
-//   return result.insertedId;
-// }
-
-// async function findUserComm(
-//   client: MongoClient,
-//   id: string
-// ): Promise<false | UserUnsafeFull> {
-//   const item = await client
-//     .db(database)
-//     .collection(userColl)
-//     .findOne({ _id: id });
-//   if (!item) {
-//     return false;
-//   }
-
-//   const userObject: UserUnsafeFull = {
-//     username: item.username,
-//     password: item.password,
-//     email: item.email,
-//     joinDate: item.joinDate,
-//     favorites: item.favorites,
-//     posts: item.posts,
-//     userImageUrl: item.userImageUrl,
-//   };
-//   return userObject;
-// }
-
-// // async function createSessionComm(
-// //   client: MongoClient,
-// //   id: string
-// // ): Promise<false | ObjectId> {
-// //   const result = await client
-// //     .db(database)
-// //     .collection('Sessions')
-// //     .insertOne({ sessionId: id });
-// //   if (!result) {
-// //     return false;
-// //   }
-// //   return result.insertedId;
-// // }
-
-// async function deleteSessionComm(client: MongoClient, id: string) {
-//   const deletion = await client
-//     .db(database)
-//     .collection('Session')
-//     .deleteOne({ _id: id });
-//   if (deletion.acknowledged) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function deleteUserComm(client: MongoClient, user: string) {
-//   const deletion = await client
-//     .db(database)
-//     .collection(userColl)
-//     .deleteOne({ username: user });
-//   if (deletion.acknowledged) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function findSessionComm(
-//   client: MongoClient,
-//   id: string
-// ): Promise<false | UserSafeFull> {
-//   const item = await client
-//     .db(database)
-//     .collection('Sessions')
-//     .findOne({ _id: id });
-//   if (!item) {
-//     return false;
-//   }
-//   const userObject: UserSafeFull = {
-//     username: item.username,
-//     email: item.email,
-//     joinDate: item.joinDate,
-//     favorites: item.favorites,
-//     posts: item.posts,
-//     userImageUrl: item.userImageUrl,
-//   };
-//   return userObject;
-// }
-
-// async function findUserByNameComm(
-//   client: MongoClient,
-//   user: string
-// ): Promise<false | UserUnsafeFull> {
-//   const item = await client
-//     .db(database)
-//     .collection(userColl)
-//     .findOne({ username: user });
-//   if (!item) {
-//     return false;
-//   }
-
-//   const userObject: UserUnsafeFull = {
-//     username: item.username,
-//     password: item.password,
-//     email: item.email,
-//     joinDate: item.joinDate,
-//     favorites: item.favorites,
-//     posts: item.posts,
-//     userImageUrl: item.userImageUrl,
-//   };
-
-//   return userObject;
-// }
-
-// async function updateDocumentComm(client: MongoClient, data: DocUpdateData) {
-//   const item = await client
-//     .db(database)
-//     .collection(data.collection)
-//     .updateOne(
-//       {
-//         [data.documentToChange.keyType]: data.documentToChange.keySearchValue,
-//       },
-//       {
-//         $set: data.dataToChange,
-//       }
-//     );
-//   console.log('updated: result_____', item);
-//   console.log(
-//     'sent request to mongo:____',
-//     {
-//       [data.documentToChange.keyType]: data.documentToChange.keySearchValue,
-//     },
-//     {
-//       $set: data.dataToChange,
-//     }
-//   );
-
-//   return item;
-// }
-//  const filter = { "mainKey.id": mainKeyId };
-// const update = { $set: { "mainKey.$": newObject } };
-// async function updateMultipleDocumentsComm(
-//   client: MongoClient,
-//   data: DocUpdateData
-// ) {
-//   const item = await client
-//     .db(database)
-//     .collection(data.collection)
-//     .updateMany(
-//       {
-//         [data.documentToChange.keyType]: data.documentToChange.keySearchValue,
-//       },
-//       {
-//         $set: data.dataToChange,
-//       }
-//     );
-//   console.log(item, 'updated: result');
-
-//   return item;
-// }
-
-// async function addToFavoritesComm(
-//   client: MongoClient,
-//   data: { user: string; data: CarObject }
-// ) {
-//   const item = await client
-//     .db(database)
-//     .collection(userColl)
-//     .updateOne({ username: data.user }, { $push: { favorites: data.data } });
-//   console.log('result from update', item);
-//   if (item && item.modifiedCount) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function addToPostsCollComm(
-//   client: MongoClient,
-//   data: { user: string; data: CarObject }
-// ) {
-//   const item = await client.db(database).collection(CarsColl).insertOne(data);
-//   console.log('result from update', item);
-//   if (item.insertedId) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function deleteFromUserPostsComm(
-//   client: MongoClient,
-//   data: { user: string; dataId: string }
-// ) {
-//   const item = await client
-//     .db(database)
-//     .collection(userColl)
-//     .updateOne(
-//       { username: data.user },
-//       {
-//         $pull: {
-//           posts: {
-//             Id: data.dataId,
-//           },
-//         },
-//       }
-//     );
-//   console.log(item);
-//   if (item && item.modifiedCount) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function deleteFromPostsCollComm(client: MongoClient, dataId: string) {
-//   const item = await client.db(database).collection(CarsColl).updateOne(
-//     { Id: dataId },
-//     {
-//       $pull: {},
-//     }
-//   );
-//   console.log(item);
-//   if (item && item.modifiedCount) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function addToUserPostsComm(
-//   client: MongoClient,
-//   data: { user: string; data: CarObject }
-// ) {
-//   const item = await client
-//     .db(database)
-//     .collection(userColl)
-//     .updateOne({ username: data.user }, { $push: { posts: data.data } });
-//   console.log('result from update', item);
-//   if (item && item.modifiedCount) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// async function deleteFromFavoritesComm(
-//   client: MongoClient,
-//   data: { user: string; dataId: string }
-// ) {
-//   const item = await client
-//     .db(database)
-//     .collection(userColl)
-//     .updateOne(
-//       { username: data.user },
-//       {
-//         $pull: {
-//           favorites: {
-//             Id: data.dataId,
-//           },
-//         },
-//       }
-//     );
-//   console.log(item);
-//   if (item && item.modifiedCount) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// export async function updateMultipleDocuments(data: DocUpdateData) {
-//   const result = await mainComm(updateMultipleDocumentsComm, data);
-//   if (result && result.modifiedCount) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// export async function createUser(data: UserUnsafeFull) {
-//   const result = await mainComm(createUserComm, data);
-//   return result;
-// }
-
-// export async function findUser(itemId: string) {
-//   const result = await mainComm(findUserComm, itemId);
-//   return result;
-// }
-
-// // export async function createSession(id: string) {
-// //   const result = await mainComm(createSessionComm, id);
-// //   return result;
-// // }
-
-// export async function findSession(id: string) {
-//   const result = await mainComm(findSessionComm, id);
-//   return result;
-// }
-
-// export async function findUserByNam(username: string) {
-//   const result = await mainComm(findUserByNameComm, username);
-
-//   return result;
-// }
-
-// export async function deleteSession(id: string) {
-//   const result = await mainComm(deleteSessionComm, id);
-//   return result;
-// }
-
-// export async function deleteUser(user: string) {
-//   const result = await mainComm(deleteUserComm, user);
-//   return result;
-// }
-
-// export async function addToFavorites(user: string, data: CarObject) {
-//   const result = await mainComm(addToFavoritesComm, { user, data });
-//   return result;
-// }
-
-// export async function deleteFromFavorites(user: string, dataId: string) {
-//   const result = await mainComm(deleteFromFavoritesComm, { user, dataId });
-//   return result;
-// }
-
-// export async function addToPostsColl(user: string, data: CarObject) {
-//   const result = await mainComm(addToPostsCollComm, { user, data });
-//   return result;
-// }
-
-// export async function addToUserPosts(user: string, data: CarObject) {
-//   const result = await mainComm(addToUserPostsComm, { user, data });
-//   return result;
-// }
-
-// export async function deleteFromPostsColl(user: string, dataId: string) {
-//   const result = await mainComm(deleteFromPostsCollComm, { user, dataId });
-//   return result;
-// }
-
-// export async function deleteFromUserPosts(user: string, dataId: string) {
-//   const result = await mainComm(deleteFromUserPostsComm, { user, dataId });
-//   return result;
-// }
 
 export function extractUserObject(data: WithId<UserUnsafeFull>) {
   const userObject: UserUnsafeFull = {
@@ -775,4 +397,26 @@ export function extractCarObject(data: WithId<CarObject>) {
     Id: data.Id,
   };
   return carObject;
+}
+
+function convertToMongoSetData(
+  type: 'post' | 'favorite' | 'user',
+  someData: { [key: string]: string }
+) {
+  let dataType: string | object;
+
+  if (type !== 'user') {
+    dataType = type === 'post' ? 'posts.$.' : 'favorites.$.';
+  } else {
+    return { $set: someData };
+  }
+
+  const dataKeys: string[] = Object.keys(someData);
+  const setDatax: { $set: { [key: string]: string } } = { $set: {} };
+
+  dataKeys.forEach((key) => {
+    const newKey = `${dataType}${key}`;
+    setDatax.$set[newKey] = someData[key];
+  });
+  return setDatax;
 }
